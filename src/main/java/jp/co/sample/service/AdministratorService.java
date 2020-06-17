94f1c111229d82ae07b2949a4dde19a5993a4933
tsudamon0421 <hikaru.tsuda@rakus.internal>
2020-06-17 13:57:22 +0900

    管理者情報サービスクラスにinsertメソッド追加

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,14 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 @Service
 @Transactional
 public class AdministratorService {
-	
+
 	@Autowired
 	private AdministratorRepository administratorRepository;
+
 	
+	/**
+	 * 管理者所法を挿入する.
+	 * 
+	 * @param administrator管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
 
 }
