9d18368b5c10f1217e40eb0c4318aaac65bfcf27
tsudamon0421 <hikaru.tsuda@rakus.internal>
2020-06-17 10:23:58 +0900

    ドメインクラスとリポジトリクラスの作成
    管理者情報を表すドメインを作成しました
    
    従業員情報ドメインを作成 管理者情報ドメインを修正
    
    AdoministratorRepositoryを生成しました
    
    EmployeeRepositoryを生成しました
    
    完成

---
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -0,0 +1,80 @@
+package jp.co.sample.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Employee;
+
+@Repository
+public class EmployeeRepository {
+
+	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
+		Employee employee = new Employee();
+		employee.setId(rs.getInt("id"));
+		employee.setName(rs.getString("name"));
+		employee.setImage(rs.getString("image"));
+		employee.setGender(rs.getString("gender"));
+		employee.setHireDate(rs.getDate("hireDate"));
+		employee.setMailAddress(rs.getString("mailAddress"));
+		employee.setZipCode(rs.getString("zipCode"));
+		employee.setAddress(rs.getString("address"));
+		employee.setTelephone(rs.getString("telephone"));
+		employee.setSalary(rs.getInt("salary"));
+		employee.setCharacteristics(rs.getString("characteristics"));
+		employee.setDependentsCount(rs.getInt("dependentsCount"));
+		return employee;
+	};
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * 従業員一覧情報を入社日(降)順で取得する (従業員が存在しない場合はサイズ0件の従業員一覧を返す).
+	 * 
+	 * @return List<Employee>
+	 */
+	public List<Employee> findAll() {
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count"
+				+ " FROM employees ORDER BY hire_date DESC;";
+
+		List<Employee> employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
+		return employeeList;
+	}
+
+	/**
+	 * 主キーから従業員情報を取得する（従業員が存在しない場合はSpringが自動的に例外を発生する).
+	 * 
+	 * @param id
+	 * @return Employee
+	 */
+	public Employee load(Integer id) {
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count"
+				+ " FROM employees WHERE id=:id;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		Employee employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
+		return employee;
+	}
+
+	/**
+	 * 従業員情報を変更する.
+	 * 
+	 * @param employee
+	 */
+	public void update(Employee employee) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+		// idカラムを除いた従業員情報のすべてのカラムを更新できるようなSQLを発行する
+		String sql = "UPDATE employees SET "
+				+ "name = :name, image = :image, gender = :gender, hire_date = :hireDate, mail_address = :mailAddress, "
+				+ "zip_code = :zipCode, address = :address, telephone = :telephone, salary = :salary, "
+				+ "characteristics = :characteristics, dependents_count =:dependentsCount WHERE id = :id;";
+		template.update(sql, param);
+	}
+
+}
