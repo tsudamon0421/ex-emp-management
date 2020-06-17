ce5967fcd2fdba396810ffde304d7b7af4152715
tsudamon0421 <hikaru.tsuda@rakus.internal>
2020-06-17 10:34:55 +0900

    従業員情報ドメインを作成 管理者情報ドメインを修正

--- src/main/java/jp/co/sample/domain/Administrator.java
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -1,51 +1,83 @@
 package jp.co.sample.domain;
 
- /**
-  * 管理者情報を表すドメイン
+/**
+ * 管理者情報を表すドメイン.
+ * 
  * @author hikaru.tsuda
  *
  */
 public class Administrator {
-	
-	/*ID*/
+
+	/* ID */
 	private Integer id;
-	/*名前*/
+	/* 名前 */
 	private String name;
-	/*メールアドレス*/
+	/* メールアドレス */
 	private String mailAddress;
-	/*パスワード*/
+	/* パスワード */
 	private String password;
+
+	/**
+	 * 引数なしのコンストラクタ.
+	 */
+	public Administrator() {
+		
+	}
 	
+	
+	/**
+	 * すべてのフィールドを引数に取るコンストラクタ.
+	 * @param id
+	 * @param name
+	 * @param mailAddress
+	 * @param password
+	 */
+	public Administrator(Integer id, String name, String mailAddress, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mailAddress = mailAddress;
+		this.password = password;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
 				+ "]";
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMailAddress() {
 		return mailAddress;
 	}
+
 	public void setMailAddress(String mailAddress) {
 		this.mailAddress = mailAddress;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 
 }
