9883bb380174244ef88f93054deb1a970ce0a041
tsudamon0421 <hikaru.tsuda@rakus.internal>
2020-06-17 15:08:31 +0900

    サービスクラスにログインメソッドの追加

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -14,7 +14,6 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
 
-	
 	/**
 	 * 管理者情報を挿入する.
 	 * 
@@ -22,6 +21,18 @@ public class AdministratorService {
 	 */
 	public void insert(Administrator administrator) {
 		administratorRepository.insert(administrator);
+	}
+
+	/**
+	 * ログイン処理をする.
+	 * 
+	 * @param mailAddres メールアドレス
+	 * @param password   パスワード
+	 * @return 管理者情報
+	 */
+	public Administrator login(String mailAddress, String password) {
+		Administrator administrator = administratorRepository.findByMailAddressAndPassword(mailAddress, password);
+		return administrator;
 	}
 
 }
