b64bc3f9b1890e66e0b6acd63bc42fafd2bfbec6
tsudamon0421 <hikaru.tsuda@rakus.internal>
2020-06-17 14:41:24 +0900

    ログイン時に使用するフォームの作成

---
+++ src/main/java/jp/co/sample/form/LoginForm.java
@@ -0,0 +1,35 @@
+package jp.co.sample.form;
+
+/**
+ * ログイン時に使用するフォーム.
+ * 
+ * @author hikaru.tsuda
+ *
+ */
+public class LoginForm {
+
+	/** メールアドレス */
+	private String mailAddress;
+	/** パスワード */
+	private String password;
+	
+	@Override
+	public String toString() {
+		return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+	}
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
